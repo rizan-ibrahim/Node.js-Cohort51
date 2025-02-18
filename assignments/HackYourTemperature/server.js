7661fb07246d5fce74c6d4023c010e8373fce521
rizan-ibrahim <rizan.ibrahim1998@gmail.com>
2025-02-18 17:13:19 +0100

    rizan-ibrahim-node.js-w1

--- assignments/HackYourTemperature/server.js
+++ assignments/HackYourTemperature/server.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { engine } from "express-handlebars";
-import fetch from "node-fetch";
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("hello from backend to frontend!");
+});
+
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+
+  if (!cityName) {
+    return res.status(400).json({ message: "City name is required." });
+  }
+
+  res.status(200).json({ cityName: cityName });
 });
 
 app.listen(PORT, () => {
