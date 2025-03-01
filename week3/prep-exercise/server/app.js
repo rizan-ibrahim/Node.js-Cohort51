fee683fa60c0a41ddcb090e86e72c15ab1b09f92
rizan-ibrahim <rizan.ibrahim1998@gmail.com>
2025-03-01 18:02:35 +0100

    server modified

--- week3/prep-exercise/server/app.js
+++ week3/prep-exercise/server/app.js
@@ -1,15 +1,21 @@
-import express from 'express';
-// TODO Use below import statement for importing middlewares from users.js for your routes
-// TODO import { ....... } from "./users.js";
+import express from "express";
+
+const PORT = 3000;
+
+import { register, login, getProfile, logout } from "./users.js";
 
 let app = express();
 
 app.use(express.json());
-// TODO: Create routes here, e.g. app.post("/register", .......)
+
+app.post("/register", register);
+app.post("/login", login);
+app.get("/getProfile", getProfile);
+app.post("/logout", logout);
 
 // Serve the front-end application from the `client` folder
-app.use(express.static('client'));
+app.use(express.static("client"));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
--- week3/prep-exercise/server/users.js
+++ week3/prep-exercise/server/users.js
@@ -1,12 +1,65 @@
-import newDatabase from './database.js'
+import newDatabase from "./database.js";
+import { v4 as uuid } from "uuid";
 
 // Change this boolean to true if you wish to keep your
 // users between restart of your application
-const isPersistent = false
-const database = newDatabase({isPersistent})
+const isPersistent = false;
+const database = newDatabase({ isPersistent });
 
 // Create middlewares required for routes defined in app.js
 // export const register = async (req, res) => {};
 
 // You can also create helper functions in this file to help you implement logic
 // inside middlewares
+export const register = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(404)
+      .json({ error: "username and password are required" });
+  }
+
+  // we check if user exist
+  const userExist = database.getById(username);
+  if (userExist) {
+    return res.status(400).json({ error: "User already exists" });
+  }
+
+  // create  new user
+  const newUser = database.create({ id: uuid(), username, password });
+  res
+    .status(201)
+    .json({ message: "User registered successfully", user: newUser });
+};
+
+// Middleware for user login
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
+  const user = database.getById(username);
+  if (!user || user.password !== password) {
+    return res.status(401).json({ error: "invaild username or password" });
+  }
+
+  res.status(200).json({ message: "Login successful", user });
+};
+
+// Middleware to get user profile
+export const getProfile = async (req, res) => {
+  const { userId } = req.params;
+  const user = database.getById(userId);
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+  res.status(200).json({ user });
+};
+
+// Middleware  logout
+export const logout = async (req, res) => {
+  res.status(200).json({ message: "Logout successful" });
+};
